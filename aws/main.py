84d56d65ac218912eed003e82aaa0676f433b7c6
Aaron Mamparo <aaronmamparo@gmail.com>
2023-03-31 20:58:29 -0500

    export bucket arn

--- aws/main.py
+++ aws/main.py
@@ -8,6 +8,7 @@ class SharedInfrastructureStack(Stack):
 		super().__init__(scope, 'soccer-analysis-shared_infrastructure')
 		bucket = Bucket(self, 'bucket', removal_policy=RemovalPolicy.DESTROY)
 		CfnOutput(self, 'bucket-output', export_name='bucket', value=bucket.bucket_name)
+		CfnOutput(self, 'bucket-arn-output', export_name='bucket-arn', value=bucket.bucket_arn)
 
 
 if __name__ == '__main__':
