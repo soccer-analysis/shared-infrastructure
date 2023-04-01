713e7a3080c155569cd454b19cd7a40a86960e8c
Aaron Mamparo <aaronmamparo@gmail.com>
2023-03-31 20:15:32 -0500

    whitelist function

--- shared_infrastructure/__init__.py
+++ shared_infrastructure/__init__.py
@@ -1,15 +1,3 @@
-# type: ignore
-# https://stackoverflow.com/a/29714764
-import os
-import pkgutil
+from shared_infrastructure.function import create_function
 
-__path__ = [x[0] for x in os.walk(os.path.dirname(__file__))]
-
-
-def load_tests(loader, suite, pattern):
-	for imp, modname, _ in pkgutil.walk_packages(__path__):
-		if not modname.endswith('_test'):
-			continue
-		for test in loader.loadTestsFromModule(imp.find_module(modname).load_module(modname)):
-			suite.addTests(test)
-	return suite
+__all__ = ['create_function']
