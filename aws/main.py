151e2de48c15ec82b9727a53b10e9aeed4875ce4
Aaron Mamparo <aaronmamparo@gmail.com>
2023-04-01 08:25:31 -0500

    auto-delete bucket objects

--- aws/main.py
+++ aws/main.py
@@ -6,7 +6,8 @@ from constructs import Construct
 class SharedInfrastructureStack(Stack):
 	def __init__(self, scope: Construct):
 		super().__init__(scope, 'soccer-analysis-shared-infrastructure')
-		data_lake_bucket = Bucket(self, 'data-lake-bucket', removal_policy=RemovalPolicy.DESTROY)
+		data_lake_bucket = Bucket(self, 'data-lake-bucket', removal_policy=RemovalPolicy.DESTROY,
+								  auto_delete_objects=True)
 		CfnOutput(self, 'data-lake-bucket-output', export_name='data-lake-bucket', value=data_lake_bucket.bucket_name)
 		CfnOutput(self, 'data-lake-bucket-arn-output', export_name='data-lake-bucket-arn',
 				  value=data_lake_bucket.bucket_arn)
